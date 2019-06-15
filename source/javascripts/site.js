9595e4cd773d1966f6093f692b799e0be472f8a7
Eduardo Higareda <eldelentes@gmail.com>
2019-06-14 20:43:34 -0500

    Project Init

---
+++ source/javascripts/site.js
@@ -0,0 +1,8 @@
+// This is where it all goes :)
+var options = {
+    valueNames: ['title', 'category', 'type', 'company', 'country', 'state'],
+    page: 20,
+    pagination: true
+};
+
+var jobList = new List('jobs', options);
