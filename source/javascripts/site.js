31d76230f213c028b22b1a33df3525423d02b129
Eduardo Higareda <eldelentes@gmail.com>
2019-06-22 14:45:05 -0500

    fix errores

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -6,3 +6,13 @@ var options = {
 };
 
 var jobList = new List('jobs', options);
+
+$("#form").submit(function(e) {
+    e.preventDefault();
+
+    var $form = $(this);
+    $.post($form.attr("action"), $form.serialize()).then(function() {
+        $('.form-content').addClass('hidden');
+        $('.form-message').removeClass('hidden');
+    });
+});
